d990066b6e01b476099076691269dad43131d2ec
AndyLegovic <andylegovic5@gmail.com>
2025-04-23 11:42:13 +0200

    Update 4.py

--- Vježba 20/4.py
+++ Vježba 20/4.py
@@ -1,8 +1,8 @@
 def jedinstveni_elementi(lista):
-    lista.sort()
     for clan in lista:
         if lista.count(clan) > 1:
-            lista.remove(clan)
+            for i in range(lista.count(clan)-1):
+                lista.remove(clan)
     return(lista)
 
 lista = ["a","a","b","a","c","d","d","d","a"]
